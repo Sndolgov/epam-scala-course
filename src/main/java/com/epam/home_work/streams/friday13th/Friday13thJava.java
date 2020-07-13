d94d4489d37f3f443ae5e16a2128018bd5219d35
sndolgov <sndolgov84@gmail.com>
2020-07-13 17:48:19 +0300

    Fixes

--- src/main/java/com/epam/home_work/streams/friday13th/Friday13thJava.java
+++ src/main/java/com/epam/home_work/streams/friday13th/Friday13thJava.java
@@ -14,15 +14,15 @@ public class Friday13thJava
         System.out.println(countFriday13th(1900, 2000));
     }
 
-    private static LinkedHashMap<Integer, Integer> countFriday13th(int startYear, int endYear)
+    private static LinkedHashMap<Integer, Long> countFriday13th(int startYear, int endYear)
     {
         return LocalDate.of(startYear, 1, 13).datesUntil(LocalDate.of(endYear, 12, 13), Period.ofMonths(1))
                 .filter(d -> d.getDayOfWeek() == DayOfWeek.FRIDAY)
-                .collect(Collectors.toMap(LocalDate::getYear, d -> 1, (o, n) -> o+=1))
+                .collect(Collectors.groupingBy(LocalDate::getYear, Collectors.counting()))
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByKey())
-                .sorted(Map.Entry.comparingByValue((a,b)->Integer.compare(b,a)))
+                .sorted(Map.Entry.comparingByValue((a,b)->Long.compare(b,a)))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o, n) -> o, LinkedHashMap::new));
     }
 }
--- src/main/java/com/epam/home_work/streams/friday13th/Friday13thScala.scala
+++ src/main/java/com/epam/home_work/streams/friday13th/Friday13thScala.scala
@@ -4,12 +4,12 @@ import java.time.{DayOfWeek, LocalDate, Period}
 
 object Friday13th {
   def main(args: Array[String]): Unit = {
-    println(countFriday13th(1900, 2000))
+    println(countFriday13thStream(1900, 2000))
 
   }
 
-    def countFriday13th(startYear: Int, endYear: Int):List[(Int,Int)] = {
-     Iterator.iterate(LocalDate.of(startYear, 1, 13))(_.plusMonths(1)).takeWhile(_.isBefore(LocalDate.of(endYear, 12, 13)))
+    def countFriday13thStream(startYear: Int, endYear: Int):List[(Int,Int)] = {
+    /* Iterator.iterate(LocalDate.of(startYear, 1, 13))(_.plusMonths(1)).takeWhile(_.isBefore(LocalDate.of(endYear, 12, 13)))
         .filter(d=> d.getDayOfWeek == DayOfWeek.FRIDAY)
        .map(_.getYear)
        .toList
@@ -17,8 +17,12 @@ object Friday13th {
        .map(e=> (e._1, e._2.size))
        .toList
        .sortBy(_._1)
-       .sortBy(-_._2)
+       .sortBy(-_._2)*/
+
+      List.range(startYear, endYear).map(year => (year, (1 to 12).count(LocalDate.of (year, _, 13).getDayOfWeek eq(DayOfWeek.FRIDAY))))
+        .sortBy(-_._2)
     }
+
 }
 
 
